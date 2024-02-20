a9479d9104961e2aeaab3ba3b06240ac139e9baa
yefrivera <yvera@unicauca.edu.co>
2024-02-20 18:36:52 -0500

    o

--- entradasal.js
+++ entradasal.js
@@ -1,5 +1,5 @@
-import * as THREE from 'three';
-import { OrbitControls } from 'https://unpkg.com/three@0.141.0/examples/jsm/controls/OrbitControls.js'; 
+import * as THREE from 'https://unpkg.com/three@0.141.0/examples/jsm/controls/OrbitControls.js';
+import { OrbitControls } from 'https://unpkg.com/three@0.141.0/examples/jsm/'; 
 
 let camera, controls;
 let renderer;
