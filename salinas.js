fa78ea4daec5dd68cb89ecb2996f8529257d211e
yefrivera <yvera@unicauca.edu.co>
2024-02-23 16:04:50 -0500

    Updates

--- salinas.js
+++ salinas.js
@@ -1,5 +1,5 @@
-import * as THREE from 'three.min.js';
-import {OrbitControls} from 'OrbitControls.js'; 
+import * as THREE from './three.min.js';
+import {OrbitControls} from './OrbitControls.js'; 
 
 let camera, controls;
 let renderer;
