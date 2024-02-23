5cb281bbce94944803adeff899571e891963c109
yefrivera <yvera@unicauca.edu.co>
2024-02-23 15:08:19 -0500

    First comit

--- dist/salinas.js
+++ dist/salinas.js
@@ -1,4 +1,4 @@
-import * as THREE from 'https://unpkg.com/three@0.141.0/examples/jsm/controls/OrbitControls.js';
+import * as THREE from 'https://unpkg.com/three@0.141.0/build/three.module.js';
 import  OrbitControls  from 'https://unpkg.com/three@0.141.0/examples/jsm/controls/OrbitControls.js'; 
 
 let camera, controls;
