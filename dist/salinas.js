979e3cbca8fe66c5875953884fd54115a49be377
yefrivera <yvera@unicauca.edu.co>
2024-02-23 16:59:45 -0500

    si

--- dist/salinas.js
+++ dist/salinas.js
@@ -1,5 +1,5 @@
-import * as THREE from 'https://unpkg.com/three@0.141.0/build/three.module.js';
-import {OrbitControls} from 'https://unpkg.com/three@0.141.0/examples/jsm/controls/OrbitControls.js'; 
+import * as THREE from 'https://unpkg.com/three@0.159.0/build/three.module.js';
+import {OrbitControls} from 'https://unpkg.com/three@0.159.0/examples/jsm/controls/OrbitControls.js'; 
 
 let camera, controls;
 let renderer;
